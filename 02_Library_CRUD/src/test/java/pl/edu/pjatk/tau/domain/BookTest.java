40a9a6afa14ecfd14a55058894a6a67eb825cb4e
Jędrzej Babło <s15715@pjwstk.edu.pl>
2019-03-05 10:01:31 +0100

    add bookTest

---
+++ 02_Library_CRUD/src/test/java/pl/edu/pjatk/tau/domain/BookTest.java
@@ -0,0 +1,24 @@
+package pl.edu.pjatk.tau.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class BookTest {
+    @Test
+    public void checkBook() {
+        Book book = new Book(1L, "Wiedzmin", 2003);
+        Assert.assertNotNull(book);
+    }
+
+    @Test
+    public void bookTest() {
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("Wiedzmin");
+        book.setYear(2003);
+        Assert.assertEquals(new Long(1), book.getId());
+        }
+}
