99ee654011b40b2fff5bb14cc611e366d3c31977
Jędrzej Babło <s15715@pjwstk.edu.pl>
2019-03-05 10:43:59 +0100

    all books test

--- 02_Library_CRUD/src/main/java/pl/edu/pjatk/tau/dao/BookInMemoryDao.java
+++ 02_Library_CRUD/src/main/java/pl/edu/pjatk/tau/dao/BookInMemoryDao.java
@@ -2,7 +2,18 @@ package pl.edu.pjatk.tau.dao;
 
 import pl.edu.pjatk.tau.domain.Book;
 import java.util.List;
+import java.util.ArrayList;
 
 public class BookInMemoryDao implements Dao<Book> {
+    public List<Book> books = new ArrayList<>();
 
+    @Override
+    public void save(Book o) {
+        books.add(o);
+    }
+
+    @Override
+    public List<Book> getAll() {
+        return books;
+    }
 }
--- 02_Library_CRUD/src/test/java/pl/edu/pjatk/tau/dao/BookInMemoryDaoTest.java
+++ 02_Library_CRUD/src/test/java/pl/edu/pjatk/tau/dao/BookInMemoryDaoTest.java
@@ -5,6 +5,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import pl.edu.pjatk.tau.domain.Book;
+
+import java.util.Collections;
 
 @RunWith(JUnit4.class)
 public class BookInMemoryDaoTest {
@@ -13,10 +16,25 @@ public class BookInMemoryDaoTest {
     @Before
     public void setup() {
         dao = new BookInMemoryDao();
+        Book b1 = new Book(1L, "Wiedzmin", 2003);
+        Book b2 = new Book(2L, "Harry Potter", 1997);
+        Collections.addAll(dao.books, b1, b2);
     }
 
     @Test
     public void createDaoObjectTest() {
         Assert.assertNotNull(dao);
+    }
+
+    @Test
+    public void checkSaving() {
+        Book b3 = new Book(3L, "Gwiezdne Wojny", 1995);
+        Assert.assertEquals(new Long(3), b3.getId());
+    }
+
+    @Test
+    public void gettingAllBooksTest(){
+        Assert.assertArrayEquals(dao.books.toArray(),dao.getAll().toArray());
+        Assert.assertEquals(dao.books.size(),dao.getAll().size());
     }
 }
