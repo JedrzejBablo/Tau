b64fdda8d788f52f45f4a1835aa05e0203a4d0d1
Creaton <creaton@macbook-air-9.home>
2019-05-15 11:56:38 +0200

    add restApi

---
+++ 08_Library_RestApi/src/main/java/pl/library/shdemo/domain/Book.java
@@ -0,0 +1,101 @@
+package pl.library.shdemo.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+@Entity(name = "Book")
+@Table(name = "Book")
+@NamedQueries({
+        @NamedQuery(name = "book.all", query = "Select b from Book b"),
+        @NamedQuery(name = "book.findBookByName", query = "Select b from Book b where b.title like :modelNameFragment"),
+        @NamedQuery(name = "book.findBooksByAuthor", query = "Select c from Book c where c.author= :author")
+})
+public class Book {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private Integer year;
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public Book() {
+    }
+
+    public Book(String title, Integer year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public Book(Long id, String title, Integer year) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) &&
+                Objects.equals(title, book.title) &&
+                Objects.equals(year, book.year);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public Book clone() {
+        Book p = new Book();
+        p.author = null;
+        p.id = id;
+        p.title = title;
+        p.year = year;
+        return p;
+    }
+
+}
---
+++ 08_Library_RestApi/src/test/java/pl/library/shdemo/service/LibraryManagerTest.java
@@ -0,0 +1,135 @@
+package pl.library.shdemo.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeNoException;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import pl.library.shdemo.domain.Book;
+import pl.library.shdemo.domain.Author;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/beans.xml"})
+//@Rollback
+@Commit
+@Transactional(transactionManager = "txManager")
+public class LibraryManagerTest {
+
+	@Autowired
+	BookManager libraryManager;
+
+	private List<Long> bookIds;
+	private List<Long> authorsIds;
+
+  /*  private Book addBookHelper(String title, Integer year) {
+        Long bookId;
+        Book book;
+        book = new Book();
+        book.setTitle(title);
+        book.setYear(year);
+        book.setId(null);
+        bookIds.add(bookId = libraryManager.addBookjkk(book));
+        assertNotNull(bookId);
+        return book;
+    }*/
+
+
+	@Before
+	public void setup() {
+		bookIds = new LinkedList<>();
+		bookIds.add(libraryManager.addBookjkk(new Book("Opowiadanie", 2003)));
+		bookIds.add(libraryManager.addBookjkk(new Book("Wiedzmin", 2005)));
+		Book book = libraryManager.findBookById(bookIds.get(0));
+		Book book1 = libraryManager.findBookById(bookIds.get(1));
+		authorsIds = new LinkedList<>();
+		authorsIds.add(libraryManager.addAuthor(new Author("Andrzej", new LinkedList<Book>(Arrays.asList(book)))));
+		authorsIds.add(libraryManager.addAuthor(new Author("Jan", new LinkedList<Book>(Arrays.asList(book1)))));
+	}
+
+	@Test
+	public void addBookTest() {
+		assertTrue(bookIds.size() > 0);
+	}
+
+	@Test
+	public void addAuthorTest() {
+		assertTrue(authorsIds.size() > 0);
+	}
+
+	@Test
+	public void getAllBooksTest() {
+		List<Long> foundIds = new LinkedList<>();
+		for (Book book : libraryManager.findAllBook()) {
+			if (bookIds.contains(book.getId())) foundIds.add(book.getId());
+		}
+		assertEquals(bookIds.size(), foundIds.size());
+	}
+
+	@Test
+	public void getBookByIdTest() {
+		Book book = libraryManager.findBookById(bookIds.get(0));
+		assertEquals("Opowiadanie", book.getTitle());
+	}
+
+	@Test
+	public void deleteBookTest() {
+		Book book = libraryManager.findBookById(bookIds.get(0));
+		libraryManager.deleteBook(book);
+		assertNull(libraryManager.findBookById(book.getId()));
+
+	}
+	@Test()
+	public void updateBookTest() {
+		Book b = libraryManager.findBookById(1L);
+		b.setTitle("Fraszka");
+		libraryManager.updateBook(b);
+		Assert.assertEquals("Fraszka",libraryManager.findBookById(1L).getTitle());
+	}
+
+	@Test
+	public void findBooksByTitleTest() {
+		List<Book> books = libraryManager.findBookByName("Opo");
+		assertEquals("Opowiadanie", books.get(0).getTitle());
+	}
+
+	@Test
+	public void findBooksByAuthor() {
+		Author author = libraryManager.findAuthorById(authorsIds.get(1));
+		List<Book> books = libraryManager.getAllBooksForAuthor(author);
+		assertEquals(1, books.size());
+	}
+
+	@Test
+	public void findAuthorByIdTest() {
+		assertEquals("Andrzej", libraryManager.findAuthorById(authorsIds.get(0)).getFirstName());
+	}
+
+	@Test
+	public void transferBookToAnotherAuthor() {
+		Book book = libraryManager.findBookById(bookIds.get(0));
+		Book book1 = libraryManager.findBookById(bookIds.get(1));
+		Author author = libraryManager.findAuthorById(authorsIds.get(0));
+		Author author1 = libraryManager.findAuthorById(authorsIds.get(1));
+		libraryManager.transferBookToAnotherAuthor(
+				book, book1, author, author1);
+		assertEquals("Wiedzmin",libraryManager.getAllBooksForAuthor(author).get(0).getTitle());
+		assertEquals(1,libraryManager.getAllBooksForAuthor(author1).size());
+
+	}
+
+}
